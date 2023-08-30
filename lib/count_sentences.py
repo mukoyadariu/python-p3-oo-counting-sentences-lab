d0534f58c77fdbcdf37045b599257d4ddf4228af
mukoyadariu <mukoya.darius@student.moringaschool.com>
2023-08-30 09:09:22 +0300

    completed assignment

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,26 @@
-#!/usr/bin/env python3
-
 class MyString:
-  pass
+    def __init__(self, value=""):
+        self.value = value
+
+    def is_sentence(self):
+        return self.value.endswith('.')
+
+    def is_question(self):
+        return self.value.endswith('?')
+
+    def is_exclamation(self):
+        return self.value.endswith('!')
+
+    def count_sentences(self):
+        if not self.value:
+            return 0
+        sentences = [s.strip() for s in self.value.split('.') if s.strip()]
+        return len(sentences)
+
+
+if __name__ == "__main__":
+    string = MyString("Hello, world!")
+    print(string.is_sentence())  # False
+    print(string.is_question())  # False
+    print(string.is_exclamation())  # True
+    print(string.count_sentences())  # 1
